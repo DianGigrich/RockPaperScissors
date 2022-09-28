761d451aea9f8c8f3f0c91ff987a9aa7aa1e09b7
Squall <112111803+DianGigrich@users.noreply.github.com>
2022-09-28 12:31:17 -0700

    rock paper scissors works, rest doesnt

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
 // collect user choice
 
-// promt user for RTCRtpSendersave user choice to a variable
+// promt user for user choice to a variable
 // edge case: adjust for casing
 // edge case: handle non-standard letters
 // edge case: handle empty submission
 // handle empty submissions
 
-// generate computer choicestore possible choices, rps;
-// randomly select item from possible choicesstore in a variable
-// compare choices
+
 // display status
 // ask to play agian
 
-var choice = prompt("Rock, Paper, Scissors, SHOOT")
+var choice = prompt("ROCK, PAPER, SCISSORS")
+
+console.log(choice)
+
+var computerOptions = ["ROCK", "PAPER", "SCISSORS"]
+
+
+var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+
+console.log(computerChoice)
+
+
+if (choice == computerChoice) {
+        console.log("Draw")
+
+// }     else if ((choice !== "ROCK", "PAPER", "SCISSORS")) {
+//             console.log("Please select appropriate choice.")
+//             Program.restart()
+             
+}   else if (((choice == "ROCK") && (computerChoice == "PAPER")) || ((choice == "SCISSORS") && (computerChoice == "ROCK")) || ((choice == "PAPER") && (computerChoice == "SCISSORS"))) {
+        console.log("Lose")
+}   else 
+console.log("win")
+
+//Play again?
+// var again = confirm("Would you like to play again?")
+
+//restart button
+//  const restartButton = documnet.getElementByID("restart")
+//  restartButton.addEventListner("click", () => {
+// var choice = prompt("Rock, Paper, Scissors, SHOOT")
+
+// console.log(choice)
+
+//  computerOptions = ["rock", "paper", "scissors"]
+
+//  computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+
+//  })
+ 
+
+// function recursion()
+// if (again == true)
+
+
+
+// } else if(computerChoice === "") {
+//     computerChoice = "scissors"
+// }
+
+
+
+// if (choice === computerChoice) {
+//     console.log("Draw")
+// }
+//     else if (choice 
+
+// }
