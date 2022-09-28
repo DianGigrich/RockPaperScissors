27b721771b1b21101bbb99fe9960c0b1531baec4
Squall <112111803+DianGigrich@users.noreply.github.com>
2022-09-28 10:19:06 -0700

    added js and html

---
+++ script.js
@@ -0,0 +1,15 @@
+// collect user choice
+
+// promt user for RTCRtpSendersave user choice to a variable
+// edge case: adjust for casing
+// edge case: handle non-standard letters
+// edge case: handle empty submission
+// handle empty submissions
+
+// generate computer choicestore possible choices, rps;
+// randomly select item from possible choicesstore in a variable
+// compare choices
+// display status
+// ask to play agian
+
+var choice = prompt("Rock, Paper, Scissors, SHOOT")
